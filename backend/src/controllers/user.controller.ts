37fcb9d572801f7fa15e64a4ee0a766c2397458c
Subhani Shaik <subhanishaik3849@gmail.com>
2026-05-09 13:12:38 +0530

    jwt changes

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,14 +1,30 @@
 import RestController from "@/helper/rest.controller.ts";
+import { hashPassword, isPasswordHashed } from "@/helper/password.helper.ts";
 import User from "@/modules/user.model.ts";
 import type { IUser } from "@/modules/user.model.ts";
 
 class UserController extends RestController<IUser, Partial<IUser>> {
   protected override readonly model = User;
 
+  protected override async preSave(
+    payload: IUser | Partial<IUser>
+  ): Promise<IUser | Partial<IUser>> {
+    const password = payload.password;
+
+    if (typeof password !== "string" || isPasswordHashed(password)) {
+      return payload;
+    }
+
+    return {
+      ...payload,
+      password: await hashPassword(password),
+    };
+  }
+
   constructor() {
     super({
-      tableName: "user",
-      schema: "user",
+      tableName: "users",
+      schema: "users",
       lookupID: "_id",
       searchAble: true,
       orderBy: "created_date",
